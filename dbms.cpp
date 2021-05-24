490ea11612ac2a127c41ee43fbbfb8f163b3d51b
malderli <ilovedarknights@gmail.com>
2021-05-24 05:13:58 +0300

    DB read v2

--- dbms.cpp
+++ dbms.cpp
@@ -2,113 +2,80 @@
 
 DBMS::DBMS(string dbFolder, bool isInit)
 {
+    // DB configuration info setup
+    dbTableStruct[TABLE_CARS] = 0xC0;
+    dbTableStruct[TABLE_MANAGERS] = 0x38;
+    dbTableStruct[TABLE_SALES] = 0x0C;
+    dbTableStruct[TABLE_CLIENTS] = 0xF0;
+
+    dbTableSize[TABLE_CARS] = 8;
+    dbTableSize[TABLE_MANAGERS] = 6;
+    dbTableSize[TABLE_SALES] = 4;
+    dbTableSize[TABLE_CLIENTS] = 8;
+
+    // Readers opening
     dbPath = dbFolder;
 
-    if (isInit) {
-        treader[TABLE_CARS].open(dbFolder + TABLE_CARS_SPATH);
-        treader[TABLE_MANAGERS].open(dbFolder + TABLE_MANAGERS_SPATH);
-        treader[TABLE_SALES].open(dbFolder + TABLE_SALES_SPATH);
-        treader[TABLE_CLIENTS].open(dbFolder + TABLE_CLIENTS_SPATH);
+    treader[TABLE_CARS].open(dbFolder + TABLE_CARS_SPATH);
+    treader[TABLE_MANAGERS].open(dbFolder + TABLE_MANAGERS_SPATH);
+    treader[TABLE_SALES].open(dbFolder + TABLE_SALES_SPATH);
+    treader[TABLE_CLIENTS].open(dbFolder + TABLE_CLIENTS_SPATH);
 
-        readDB();
-    }
-    else {
-
-    }
+    readDB();
 }
 
 void DBMS::readDB()
 {
     string str;
-    rowData currData;
+    rowData *currData;
 
     for (int i = 0; i < NUM_OF_TABLES; i++)
         tdata[i].clear();
 
-    // TABLE_CARS reading
-    currData.tableID = TABLE_CARS;
+    for (int tid = 0; tid < NUM_OF_TABLES; tid++)
+        while (!treader[tid].eof()) {
+            currData = new rowData();
+            currData->tableID = tid;
 
-    while (!treader[TABLE_CARS].eof()) {
-        getline(treader[TABLE_CARS], str); currData.ints.push_back(stoi(str)); // ID
-        getline(treader[TABLE_CARS], str); currData.ints.push_back(stoi(str)); // PRICE
-        getline(treader[TABLE_CARS], str); currData.strings.push_back(str);    // MODEL
-        getline(treader[TABLE_CARS], str); currData.strings.push_back(str);    // YEAR
-        getline(treader[TABLE_CARS], str); currData.strings.push_back(str);    // COLOR
-        getline(treader[TABLE_CARS], str); currData.strings.push_back(str);    // TECH
-        getline(treader[TABLE_CARS], str); currData.strings.push_back(str);    // EQUIP
-        getline(treader[TABLE_CARS], str); currData.strings.push_back(str);    // COMMENT
+            for (int col = 0; col < dbTableSize[tid]; col++) {
+                getline(treader[tid], str);
 
-        tdata[TABLE_CARS].push_back(currData);
-        currData.strings.clear();
-        currData.strings.clear();
-    }
-
-    // TABLE_MANAGERS reading
-    currData.tableID = TABLE_MANAGERS;
-    
-    while (!treader[TABLE_MANAGERS].eof()) {
-        getline(treader[TABLE_MANAGERS], str); currData.ints.push_back(stoi(str)); // ID
-        getline(treader[TABLE_MANAGERS], str); currData.ints.push_back(stoi(str)); // SUMDEALS
-        getline(treader[TABLE_MANAGERS], str); currData.ints.push_back(stoi(str)); // TOTALEARN
-        getline(treader[TABLE_MANAGERS], str); currData.strings.push_back(str);    // NAME
-        getline(treader[TABLE_MANAGERS], str); currData.strings.push_back(str);    // BIRTHDATE
-        getline(treader[TABLE_MANAGERS], str); currData.strings.push_back(str);    // COMMENT
-
-        tdata[TABLE_MANAGERS].push_back(currData);
-        currData.strings.clear();
-        currData.strings.clear();
-    }
-
-    // TABLE_SALES reading
-    currData.tableID = TABLE_SALES;
-
-    while (!treader[TABLE_SALES].eof()) {
-        getline(treader[TABLE_SALES], str); currData.ints.push_back(stoi(str)); // ID  
-        getline(treader[TABLE_SALES], str); currData.ints.push_back(stoi(str)); // CARID
-        getline(treader[TABLE_SALES], str); currData.strings.push_back(str);    // DATE
-        getline(treader[TABLE_SALES], str); currData.strings.push_back(str);    // COMMENT
-
-        tdata[TABLE_SALES].push_back(currData);
-        currData.strings.clear();
-        currData.strings.clear();
-    }
-
-    // TABLE_CLIENTS reading
-    currData.tableID = TABLE_CLIENTS;
-
-    while (!treader[TABLE_CLIENTS].eof()) {
-        getline(treader[TABLE_CLIENTS], str); currData.ints.push_back(stoi(str)); // ID
-        getline(treader[TABLE_CLIENTS], str); currData.ints.push_back(stoi(str)); // PHONE
-        getline(treader[TABLE_CLIENTS], str); currData.ints.push_back(stoi(str)); // STAT
-        getline(treader[TABLE_CLIENTS], str); currData.ints.push_back(stoi(str)); // DISCOUNT  
-        getline(treader[TABLE_CLIENTS], str); currData.strings.push_back(str);    // NAME
-        getline(treader[TABLE_CLIENTS], str); currData.strings.push_back(str);    // BIRTHDATE
-        getline(treader[TABLE_CLIENTS], str); currData.strings.push_back(str);    // PASSDATA
-        getline(treader[TABLE_CLIENTS], str); currData.strings.push_back(str);    // COMMENT
-
-        tdata[TABLE_CLIENTS].push_back(currData);
-        currData.strings.clear();
-        currData.strings.clear();
-    }
+                if ((dbTableStruct[tid] >> col) & 0x01)
+                    currData->ints.push_back(stoi(str));
+                else
+                    currData->strings.push_back(str);
+            }
+        }
 }
 
 void DBMS::saveDB()
 {
     for (int tid = 0; tid < NUM_OF_TABLES; tid++)
-        for (rowData currData : tdata[tid]) {
-            for (int i : currData.ints)
+        for (rowData *currData : tdata[tid]) {
+            for (int i : currData->ints)
                 treader[tid] << i << '\n';
-            for (string s : currData.strings)
+            for (string s : currData->strings)
                 treader[tid] << s << '\n';
         }
 }
 
 void DBMS::ADD(rowData data)
 {
-    tdata[data.tableID].push_back(data);
+    tdata[data.tableID].push_back(&data);
 }
 
-rowData DBMS::GET(rowData data)
+vector<rowData*> DBMS::GET(rowData toGet)
 {
-    
+    vector<rowData*> res;
+
+    if (toGet.tableID != TABLE_SALES)
+        for (rowData *currData : tdata[toGet.tableID])
+                if (currData->strings[0] == toGet.strings[0])
+                    res.push_back(currData);
+    else
+        for (rowData *currData : tdata[toGet.tableID])
+                if (currData->ints[0] == toGet.ints[0])
+                    res.push_back(currData);
+
+    return res;
 }
--- dbms.h
+++ dbms.h
@@ -9,19 +9,22 @@ using namespace std;
 class DBMS
 {
 private:
-    bool initDB(string dbFolder);
+    // bool initDB(string dbFolder);
     void readDB();
     void saveDB();
 
+    int dbTableStruct[NUM_OF_TABLES];
+    short dbTableSize[NUM_OF_TABLES];
+
     fstream treader[NUM_OF_TABLES];
-    vector<rowData> tdata[NUM_OF_TABLES];
+    vector<rowData*> tdata[NUM_OF_TABLES];
 
 public:
     string dbPath;
 
     DBMS(string dbFolder, bool isInit);
 
-    rowData GET(rowData data);
+    vector<rowData*> GET(rowData toGet);
     void ADD(rowData data);
     bool REMOVE(rowData data);
 };
